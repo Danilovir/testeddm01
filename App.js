d7d39e0d67788772068a2999ae31b780a87e1647
Danilovir <danilo.vieira7@fatec.sp.gov.br>
2024-03-04 11:46:18 -0300

    Primeiro commit clone 3b3 Lado A

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Danilo Rodrigues </Text>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Clone do projeto aula 04-03-2024 3B3 Lado A</Text>
       <StatusBar style="auto" />
     </View>
   );
