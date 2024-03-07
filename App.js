0eae4adb6a2addaa4f18a91d6d31e9f198e18cc6
Danilovir <danilo.vieira7@fatec.sp.gov.br>
2024-03-07 10:48:18 -0300

    Rota

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Title from './src/components/';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Danilo Rodrigues </Text>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Clone do projeto aula 04-03-2024 3B3 Lado A</Text>
+<Title/>
       <StatusBar style="auto" />
     </View>
   );
